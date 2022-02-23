1cbbabfa3fb218233c05c26038446ad32ef36b70
dimondrive <48328754+dimondrive@users.noreply.github.com>
2022-02-23 22:12:22 +0200

    new site

---
+++ js/script.js
@@ -0,0 +1,47 @@
+
+// let a=19;
+// const button = document.querySelector('button');
+// const input = document.querySelector('.age');
+
+// () => стрелочная функция
+// button.onclick = () => {
+//    let num=+input.value;
+//    if(num>=16)
+//    {
+//        console.log('welcome')
+//    }
+//    else
+//    {
+//        console.log('ты не пройдешь')
+//    }
+
+// }
+
+
+// console.log('Дима');
+// console.log(3);
+// console.log('Добро'+' '+'пожаловать '+' курс');
+// document.getElementById('one').innerHTML='hello world';
+// document.querySelector('#one').innerHTML='ghe'+'gefe';
+// document.getElementById('two').innerHTML=12*12;
+// document.querySelector('.one').innerHTML="Hello World";
+// document.querySelector('span').innerHTML="World";
+// document.querySelector('.three').innerHTML='<h3>asdzxc</h3>';
+// document.querySelector('.four').innerHTML+='<h4>asd</h4>'+'<p>pasd</p>';
+// let a=document.querySelector('.five');
+// a.innerHTML=3.14;
+// let div7=document.querySelector('.seven');
+// div7.innerHTML='<img src="https://cdn4.iconfinder.com/data/icons/food-and-drink-flat-gradient/32/cone_ice_cream_food_drink_sweet-512.png" alt="">';
+// let z1=6;
+//let z2=3;
+//document.querySelector('.nine').innerHTML=z1*z2;
+//let x1='hello';
+//let x2=5;
+//document.querySelector('.ten').innerHTML='x1'+'x2';
+//let d1=document.querySelector('.test-1');
+//console.log(d1);
+//let d2=document.querySelector('.test-2');
+//console.log(d2);
+//d2=5;
+//console.log(d2);
+
